a877c219228a68e4b7ae9eada74e1e42e64ce2cd
bcolb <brice.tech@gmail.com>
2013-10-15 00:24:32 -0400

    Resolved regular expression issue in AbbreviationWriter

--- AbbreviationWriter.java
+++ AbbreviationWriter.java
@@ -3,15 +3,23 @@ import java.util.HashMap;
 
 public class AbbreviationWriter extends FilterWriter{
 
-    private HashMap map;
+    private HashMap<String, String> map;
 
-    public AbbreviationWriter(Writer out, HashMap map){
+    public AbbreviationWriter(Writer out, HashMap<String, String> map){
 	super(out);
 	this.map = map;
+	System.out.println(map);
     }
 
     public void write(String str) throws IOException {
-	out.write(str);
-	// Overwrites FilterWriter's write(String str) method
+	String[] parts = str.split("\\s+");
+	for(int i = 0; i < parts.length; i++){
+	    if(map.containsKey(parts[i])){
+		out.write(map.get(parts[i]) + " ");
+	    } else {
+		out.write(parts[i] + " ");
+	    }
+	}
+	out.write("\n");
     }
 }
--- TestAbbreviationWriter.java
+++ TestAbbreviationWriter.java
@@ -1,4 +1,12 @@
-import java.io.*;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Class name: TestAbbreviationWriter
@@ -8,17 +16,54 @@ import java.io.*;
  * Two Writer objects are created and wrapped in the AbbreviationWriter.
  */
 public class TestAbbreviationWriter{
-    
-    // Fields
-    private BufferedWriter bufferedWriter;
-    private StringWriter stringWriter;
+
+    final String OUTPUT_FILE_NAME = "output.txt";
+    HashMap<String, String> abbMap;
+    Scanner abbScan; // Abbreviations File Scanner
+    Scanner inScan;  // Input File Scanner
     
     /**
      * Method name: TestAbbreviationWriter
      */
-    public TestAbbreviationWriter(){
-	// bufferedWriter = new BufferedWriter();
-	stringWriter = new StringWriter();
+    public TestAbbreviationWriter(String abbFile, String inFile){
+	abbMap = new HashMap<String, String>();
+
+	// Opening up the abbreviations file scanner
+	try { 
+	    abbScan = new Scanner(new BufferedReader(new FileReader(abbFile)));
+	} catch (FileNotFoundException e) {
+	    printUsageAndExit();
+	} 
+
+	// Creating a hashmap from the abbreviations scanner
+	while(abbScan.hasNext()){
+	    String[] parts = (abbScan.nextLine()).split("\\s+", 2);
+	    abbMap.put(parts[0], parts[1]);
+	}
+	abbScan.close();
+
+	// Opening up the text file
+	try {
+	    inScan = new Scanner(new BufferedReader(new FileReader(inFile)));
+	} catch (FileNotFoundException e) {
+	    printUsageAndExit();
+	}
+
+	try {
+	    File file = new File(OUTPUT_FILE_NAME);
+	    if(!file.exists()) file.createNewFile();
+	    BufferedWriter buffWriter = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+	    AbbreviationWriter abbWriter = new AbbreviationWriter(buffWriter, abbMap);
+	    // Conduct testing of the AbbreviationWriter
+	    while(inScan.hasNext()) {
+		String line = inScan.nextLine();
+		abbWriter.write(line);
+	    }
+	    abbWriter.close();
+	} catch (IOException e){
+	    e.printStackTrace();
+	}
+	inScan.close();
     }
     
     /**
@@ -27,12 +72,17 @@ public class TestAbbreviationWriter{
     public static void printUsageAndExit(){
 	System.out.println("***** TestAbbreviationWriter *****");
 	System.out.println("Requires two command line arguments:");
-	System.out.println("- First argument is a file containg a list of abbreviations");
-	System.out.println("- Second argument is the name of the text file to be written/read");
-	System.out.println("Example: java TestAbbreviationWriter abbreviations.txt testfile.txt");
+	System.out.println("1) The name of a file containg a list of abbreviations.");
+	System.out.println("2) The name of the file to be read/written to.");
+	System.out.println("Example: java TestAbbreviationWriter abbreviations.txt paragraph.txt");
+	System.exit(0);
     }
 
+    /**
+     *
+     */
     public static void main(String[] args){
-	printUsageAndExit();
+	if(args.length < 2) printUsageAndExit();
+	TestAbbreviationWriter testWriter = new TestAbbreviationWriter(args[0], args[1]);
     }
 }
