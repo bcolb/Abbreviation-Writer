a00c54b092a46a7d889652bc7be30ff50f686048
bcolb <brice.tech@gmail.com>
2013-10-16 22:38:57 -0400

    Added test files, simplified test class

--- AbbreviationWriter.java
+++ AbbreviationWriter.java
@@ -1,25 +1,65 @@
-import java.io.*;
+/**
+ * AbbreviationWriter.java
+ * 
+ * @author Brice Colbert
+ * @version 16 OCT 2013
+ */
+import java.io.IOException;
+import java.io.Writer;
+import java.io.FilterWriter;
 import java.util.HashMap;
 
+/**
+ * Class name: AbbreviationWriter
+ * Inheritance: FilterWriter
+ * Attributes: HashMap<String, String> map
+ * Methods: AbbreviationWriter(Writer out, HashMap map), Write(String str)
+ * Functionality: A decorator class that can wrap any java Writer class,
+ *                AbbreviationWriter checks each input string passed to
+ *                the the Write method against keys in its HashMap. If the
+ *                key is found in the Hashmap, then that value in the String
+ *                is replaced by associated value in the HashMap and written
+ *                out. 
+ * Visibility: public	  
+ */
 public class AbbreviationWriter extends FilterWriter{
 
     private HashMap<String, String> map;
 
+    /**
+     * Name: AbbreviationWriter
+     * Functionality: Initializes the AbbreviationWriter from the 
+     *                Writer and HashMap that are passed in as parameters
+     * Visibility: public
+     * @param Writer out
+     * @param HashMap<String, String> map
+     */
     public AbbreviationWriter(Writer out, HashMap<String, String> map){
 	super(out);
 	this.map = map;
-	System.out.println(map);
     }
 
+    /**
+     * Name: write
+     * Inheritance: Uses the write(String str) method from the writer
+     *              object it is decorating.
+     * Functionality: Filters the String passed in as a parameter by checking
+     *                for any of the abbreviations in the hashmap. If an 
+     *                abbreviation is found, it is replaced by its  
+     *                corresponding value.   
+     * @param String str
+     */
     public void write(String str) throws IOException {
-	String[] parts = str.split("\\s+");
+	String[] parts = str.split("(\\s+)");
 	for(int i = 0; i < parts.length; i++){
-	    if(map.containsKey(parts[i])){
-		out.write(map.get(parts[i]) + " ");
+	    String key = parts[i].toLowerCase();
+	    if(map.containsKey(key)){
+		out.write(map.get(key) + " ");
 	    } else {
 		out.write(parts[i] + " ");
 	    }
 	}
 	out.write("\n");
     }
+
 }
--- TestAbbreviationWriter.java
+++ TestAbbreviationWriter.java
@@ -1,86 +1,132 @@
+/**
+ * TestAbbreviationWriter.java
+ *
+ * @author Brice Colbert
+ * @version 16 OCT 2013
+ */
+
 import java.util.Scanner;
 import java.util.HashMap;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Class name: TestAbbreviationWriter
- * Description: The TestAbbreviationWriter class takes as input two arguments
- * from the command line - The name of a file where abbreviations are defined
- * and the name of the text file to be read/written to.
- * Two Writer objects are created and wrapped in the AbbreviationWriter.
+ * Inheritance: none
+ * Functionality: The TestAbbreviationWriter class takes as input two arguments
+ *                from the command line - The name of a file where abbreviations
+ *                are defined and the name of the text file to be read/written 
+ *                to. Two Writer objects are created and wrapped in the 
+ *                AbbreviationWriter.
+ * Visibility: public
  */
 public class TestAbbreviationWriter{
 
-    final String OUTPUT_FILE_NAME = "output.txt";
-    HashMap<String, String> abbMap;
-    Scanner abbScan; // Abbreviations File Scanner
-    Scanner inScan;  // Input File Scanner
-    
+    final String OUTPUT_FILE_NAME = "outfile.txt"; // File to be written out
+    private HashMap<String, String> abbMap; // abbreviation definitions
+    private Scanner abbScan; // Abbreviations File Scanner
+    private Scanner inScan;  // Input File Scanner
+
     /**
      * Method name: TestAbbreviationWriter
+     * Functionality: Initializes the Test environment state and runs the
+     *                accompanying test. All additional methods in the 
+     *                class are simple helper methods for this constructor
+     *                to make the class more readable.
+     * @param String abbFile
+     * @param String inFile
      */
     public TestAbbreviationWriter(String abbFile, String inFile){
-	abbMap = new HashMap<String, String>();
-
-	// Opening up the abbreviations file scanner
+	// Open up the scanners and initialize the hashmap
 	try { 
 	    abbScan = new Scanner(new BufferedReader(new FileReader(abbFile)));
+	    inScan = new Scanner(new BufferedReader(new FileReader(inFile)));
+	    abbMap = populateHashMap(abbScan, "@");
+	    abbScan.close();
 	} catch (FileNotFoundException e) {
 	    printUsageAndExit();
 	} 
-
-	// Creating a hashmap from the abbreviations scanner
-	while(abbScan.hasNext()){
-	    String[] parts = (abbScan.nextLine()).split("\\s+", 2);
-	    abbMap.put(parts[0], parts[1]);
-	}
-	abbScan.close();
-
-	// Opening up the text file
-	try {
-	    inScan = new Scanner(new BufferedReader(new FileReader(inFile)));
-	} catch (FileNotFoundException e) {
-	    printUsageAndExit();
-	}
-
+	
+	// Initializes the writers, reads the input by line, writes the output 
 	try {
 	    File file = new File(OUTPUT_FILE_NAME);
 	    if(!file.exists()) file.createNewFile();
-	    BufferedWriter buffWriter = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
-	    AbbreviationWriter abbWriter = new AbbreviationWriter(buffWriter, abbMap);
-	    // Conduct testing of the AbbreviationWriter
-	    while(inScan.hasNext()) {
-		String line = inScan.nextLine() + "\n";
-		System.out.print(line);
-		// abbWriter.write(line);
+	    AbbreviationWriter abbWriter1 = new AbbreviationWriter(new BufferedWriter(new FileWriter(file.getAbsoluteFile())), abbMap);
+	    StringWriter stringWriter = new StringWriter();
+	    AbbreviationWriter abbWriter2 = new AbbreviationWriter(stringWriter, abbMap); 
+	    while(inScan.hasNext()) {             
+		String line = inScan.nextLine();  // Reads input from the file line by line
+		abbWriter1.write(line);           // Writes to the FileWriter
+		abbWriter2.write(line);           // Writes to the StringWriter
 	    }
-	    abbWriter.close();
+	    System.out.println("" + stringWriter.toString());
+	    abbWriter1.close();
+	    abbWriter2.close();
+	    inScan.close();
 	} catch (IOException e){
 	    e.printStackTrace();
 	}
-	inScan.close();
     }
+
+    /**
+     * Method name: populateHashMap
+     * Functionality: Scans a file for abbreviations in the format 
+     *                ABB abbreviation
+     *                Populates a HashMap with the abbreviations as keys
+     *                and the expansions as values
+     * Visibility: private
+     * @param Scanner scan
+     * @return HashMap<String, String>
+     */
+    private HashMap<String, String> populateHashMap(Scanner scan){
+	HashMap<String, String> newMap = new HashMap<String, String>();
+	while(scan.hasNext()){
+	    String[] parts = (scan.nextLine()).split("\\s+", 2);
+	    newMap.put((parts[0]).toLowerCase(), parts[1]);
+	}
+	return newMap;
+    }
+
+    /**
+     * Method name: populateHashMap
+     * Functionality: Scans a file for abbreviations in the format 
+     *                ABB abbreviation
+     *                Populates a HashMap with the abbreviations as keys
+     *                and the expansions as values. Prefixes the keys with
+     *                the provided String delimiter.
+     * Visibility: private
+     * @param Scanner scan
+     * @param String delimiter
+     * @return HashMap<String, String>
+     */
+    private HashMap<String, String> populateHashMap(Scanner scan, String delimiter){
+	HashMap<String, String> newMap = new HashMap<String, String>();
+	while(scan.hasNext()){
+	    String[] parts = (scan.nextLine()).split("\\s+", 2);
+	    newMap.put((delimiter+parts[0]).toLowerCase(), parts[1]);
+	}
+	return newMap;
+    }    
     
     /**
      * Method name: printUsageAndExit
+     * Functionality: Prints instructions for using the test class.
+     * Visibility: public
      */
     public static void printUsageAndExit(){
 	System.out.println("***** TestAbbreviationWriter *****");
 	System.out.println("Requires two command line arguments:");
 	System.out.println("1) The name of a file containg a list of abbreviations.");
 	System.out.println("2) The name of the file to be read/written to.");
-	System.out.println("Example: java TestAbbreviationWriter abbreviations.txt paragraph.txt");
+	System.out.println("Example: java TestAbbreviationWriter chatacronyms.txt chat.txt");
 	System.exit(0);
     }
 
     /**
-     *
+     * Method name: main
+     * Inheritance: none
+     * Functionality: Checks that at least two arguments were passed. Creates
+     *                a new test object to start the testing of AbbreviationWriter
+     * @param String[] args
      */
     public static void main(String[] args){
 	if(args.length < 2) printUsageAndExit();
